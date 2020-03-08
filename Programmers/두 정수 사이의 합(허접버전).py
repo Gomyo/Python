9d8472890d47f84b4d01e8ab5847c8792076b223
Gomyo <cjw1102@naver.com>
2020-03-08 23:44:20 +0900

    Add solution

--- Programmers/두 정수 사이의 합(허접버전).py
+++ Programmers/두 정수 사이의 합(허접버전).py
@@ -9,3 +9,9 @@ def solution(a, b):
         for j in range(a,b+1):
             answer += j
     return answer
+
+# 다른 사람의 답
+def solution(a, b):
+    if a > b: a, b = b, a
+
+    return sum(range(a,b+1))
