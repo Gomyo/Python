5b26d123449c0523de168c6b5d1d7768f574fa74
Gomyo <cjw1102@naver.com>
2020-03-10 10:33:23 +0900

    Update 막쓰는파일.py

--- Programmers/막쓰는파일.py
+++ Programmers/막쓰는파일.py
@@ -1,14 +1,23 @@
-s = '1234'
+s = '-1234'
 
+# 나의 풀이
 def solution(s):
-    ls = len(s)
-    if ls == 4 or 6:
-        try:
-            int(s)
-            return True
-        except ValueError:
-            return False
+    if s[0] == '-':
+        return -int(s[1:])
+    elif s[0] == '+':
+        return int(s[1:])
     else:
-        return False 
+        return int(s)
 
-print(solution(s))
+# 다른 사람의 풀이
+def strToInt(str):
+    result = 0
+
+    for idx, number in enumerate(str[::-1]):
+        if number == '-':
+            result *= -1
+        else:
+            result += int(number) * (10 ** idx)
+
+    return result
+
