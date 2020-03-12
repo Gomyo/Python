bdcd9d82c19734bca7219f2dc3fb190cf1b335bd
gomyo <cjw1102@naver.com>
2020-03-12 18:27:21 +0900

    fix: Change dir name

--- GroomLevel/[1]3과 5의 배수.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-a = 0
-
-for x in range(n+1):
-    if x%3 == 0 or x%5 == 0:
-        a += x
-print(a)
--- GroomLevel/[1]약수 구하기.py
+++
@@ -1,9 +0,0 @@
-# 정답!
-n = int(input())
-result = []
-
-for x in range(1,n+1):
-    if n%x == 0:
-        result.append(x)
-for i in result:
-    print(i,end=' ')
