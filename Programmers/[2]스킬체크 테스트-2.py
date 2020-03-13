fdc395547496aaa6252fde47aceb1cc96eb2d42d
gomyo <cjw1102@naver.com>
2020-03-13 22:28:43 +0900

    Study

--- Programmers/[2]스킬체크 테스트-2.py
+++ Programmers/[2]스킬체크 테스트-2.py
@@ -1,2 +1,33 @@
 def solution(n):
-    
+    base = ['A']*len(n)
+    ans = 0
+    n = list(n)
+    idx = 0
+    while True:
+        rightidx = 1
+        leftidx = 1
+        if n[idx] != 'A':
+            if ord(n[idx])-ord('A') >= 14:
+                ans += 26 - (ord(n[idx])-ord('A'))
+            else:
+                ans += ord(n[idx])-ord('A')
+            n[idx] = "A"
+        if n == base:
+            break
+        else:
+            for i in range(1,len(n)):
+                if n[idx+i] == 'A':
+                    rightidx+=1
+                else:
+                    break
+                if n[idx-i] == 'A':
+                    leftidx+=1
+                else:
+                    break
+            if rightidx > leftidx:
+                ans += leftidx
+                idx -= leftidx
+            else:
+                ans += rightidx
+                idx += rightidx
+    return ans
